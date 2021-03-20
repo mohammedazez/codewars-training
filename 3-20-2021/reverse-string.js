46ebad7bde3255e85a8aa4c30637baa9097a4b76
mohammedazez <muhamadaziz047@gmail.com>
2021-03-20 22:01:45 +0700

    add reverse string

--- 3-20-2021/reverse-string.js
+++ 3-20-2021/reverse-string.js
@@ -1,8 +1,16 @@
 function solution(str) {
-  return str;
+  console.log(str);
+  let word = "";
+  console.log(word);
+  for (let i = str.length - 1; i >= 0; i--) {
+    word += str[i];
+    console.log(str[i]);
+    console.log(word);
+  }
+  return word;
 }
 
 console.log(solution("world"), "dlrow");
-console.log(solution("hello"), "olleh");
-console.log(solution(""), "");
-console.log(solution("h"), "h");
+// console.log(solution("hello"), "olleh");
+// console.log(solution(""), "");
+// console.log(solution("h"), "h");
