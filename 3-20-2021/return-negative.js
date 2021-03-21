55c2d82e38215a94cf4d0f8a82f704db93d3910b
mohammedazez <muhamadaziz047@gmail.com>
2021-03-21 10:20:33 +0700

    add return negative

--- 3-20-2021/return-negative.js
+++ 3-20-2021/return-negative.js
@@ -1,13 +1,17 @@
-function makeNegative(num) {
-  console.log(num);
-  if (num > 0) {
-    return -num;
-  } else {
-    return num;
+function makeNegative(number) {
+  console.log(number);
+  if (number >= 0) {
+    return -Math.abs(number);
   }
+
+  if (number < 0) {
+    return number;
+  }
+  return number;
 }
 
 console.log(makeNegative(1));
 console.log(makeNegative(-5));
 console.log(makeNegative(0));
 console.log(makeNegative(0.12)); // return -1 // return -5 // return 0
+console.log(makeNegative(42), -42);
