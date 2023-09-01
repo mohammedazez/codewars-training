0addb88ac08a04baaae6dad78b9b2d348c99d99f
mohammedazez <muhamadaziz047@gmail.com>
2023-09-01 12:00:02 +0700

    two sum

--- golang/questions/twoSum.go
+++ golang/questions/twoSum.go
@@ -1,8 +1,11 @@
 package questions
 
+// []int{2, 7, 11, 15}, 9
 // 2, 7, 11, 15 					-> FIRST LOOPING
 // 7 ,11, 15     11, 15      15 	-> SECOND LOOPING
-//
+// apakah 7 sama dengan 9 - 2 --> 7 == 7 jadi iya
+// apakah 11 sama dengan 9 - 7 --> 2 == 11 jadi tidak
+// apakah 15 sama dengan 9 - 15 --> 6 == 15 jadi tidak
 
 func TwoSum(nums []int, target int) []int {
 	var res []int
