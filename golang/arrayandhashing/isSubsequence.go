13fe63d439aed8dfbf94014cedb8b9a14255b64b
mohammedazez <muhamadaziz047@gmail.com>
2023-09-07 11:30:37 +0700

    IsSubsequence

---
+++ golang/arrayandhashing/isSubsequence.go
@@ -0,0 +1,34 @@
+package arrayandhashing
+
+import (
+	"strings"
+)
+
+func IsSubsequence(s string, t string) bool {
+	wordS := strings.Split(s, "")
+	wordT := strings.Split(t, "")
+	var res bool
+
+	i := 0
+	for i < len(wordS) {
+		j := i
+		for j < len(wordT) {
+			if wordS[i] == wordT[j] {
+				break
+			}
+			if wordS[i] != wordT[j] {
+				wordT = append(wordT[:j], wordT[j+1:]...)
+				j -= 1
+			}
+			j++
+		}
+		i++
+	}
+	if strings.Join(wordT, "") == s {
+		res = true
+	} else if s == "" && t != "" {
+		res = true
+	}
+
+	return res
+}
